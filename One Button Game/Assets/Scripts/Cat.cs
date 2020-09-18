66ba616a71d04f4b2f3ccc8f26b0032d89b2fb45
Gen <geneva@glowup.games>
2020-09-18 19:13:35 -0400

    Committing the actual project nowww
    about to start prototyping ideas

---
+++ One Button Game/Assets/Scripts/Cat.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cat", menuName = "Custom Class/Cat", order = 1)]
+public class Cat : ScriptableObject
+{
+    public string _name;
+    public float sniffTime,petTime,bowlAmt;
+    public bool doesLikeWiggles;
+    public int daysTillStay;
+
+    public Animator myAnim;
+
+}
---
+++ One Button Game/Assets/Scripts/Homie.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Homie",menuName ="Custom Class/Homie",order = 0)]
+public class Homie : ScriptableObject
+{
+    public string _name;
+    public float kissTime;
+    public Animator myAnim;
+    public enum Action
+    {
+        lookAway,
+        pointFingers,
+        squint
+    };
+
+    public Action myAction;
+
+}
+
